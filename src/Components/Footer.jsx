ee6346b62144bc7dc23f8782fb5a58781becb5a1
Abhishek <abhisaloke2424@gmail.com>
2024-02-14 15:04:47 +0530

    errors resolved

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,15 @@
 
 function Footer() {
   return (
-    <div class="card text-center bg-dark text-light">
-        <div class="card-body">
-            <h5 class="card-title text-warning fs-4">News_Dekho.com</h5>
-            <p class="card-text">Latest News for your knowledge that keep you upto date</p>
+    <div className="card text-center bg-dark text-light">
+        <div className="card-body">
+            <h5 className="card-title text-warning fs-4">News_Dekho.com</h5>
+            <p className="card-text">Latest News for your knowledge that keep you upto date</p>
         </div>
-        <div class="card-footer text-light">
+        <div className="card-footer text-light">
             &copy; 2024  News_Dekho.com all rights reserved
         </div>
-        <div class="card-footer  text-white fs-sm">
+        <div className="card-footer  text-white fs-sm">
             Devloped by Abhishek Salokhe with &#10084;
         </div>
     </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ function Navbar({setcategory,setCountry}) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#"><span class="badge rounded-pill text-bg-warning fs-4">News_Dekho</span></a>
+            <a className="navbar-brand" href="#"><span className="badge rounded-pill text-bg-warning fs-4">News_Dekho</span></a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
@@ -28,21 +28,21 @@ function Navbar({setcategory,setCountry}) {
                   <div className="nav-link" onClick={()=>setcategory("entertainment")} style={{color:"yellow"}}>Entertainment</div>
                </li>
             </ul>
-            <div class="dropdown">
-  <button class="btn btn-warning dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <div className="dropdown">
+  <button className="btn btn-warning dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
     Select Country
   </button>
-  <ul class="dropdown-menu">
-      <li><div class="dropdown-item" onClick={()=>{setCountry("IN")}} style={{cursor:"pointer"}}>India</div></li>
-      <li><div class="dropdown-item" onClick={()=>{setCountry("US")}} style={{cursor:"pointer"}}>United States</div></li>
-      <li><div class="dropdown-item" onClick={()=>{setCountry("GB")}} style={{cursor:"pointer"}}>United Kingdom</div></li>
-      <li><div class="dropdown-item" onClick={()=>{setCountry("CA")}} style={{cursor:"pointer"}}>Canada</div></li>
-      <li><div class="dropdown-item" onClick={()=>{setCountry("DE")}} style={{cursor:"pointer"}}>Germany</div></li>
-      <li><div class="dropdown-item" onClick={()=>{setCountry("FR")}} style={{cursor:"pointer"}}>France</div></li>
-      <li><div class="dropdown-item" onClick={()=>{setCountry("IT")}} style={{cursor:"pointer"}}>Italy</div></li>
-      <li><div class="dropdown-item" onClick={()=>{setCountry("AU")}} style={{cursor:"pointer"}}>Australia</div></li>
-      <li><div class="dropdown-item" onClick={()=>{setCountry("JP")}} style={{cursor:"pointer"}}>Japan</div></li>
-      <li><div class="dropdown-item" onClick={()=>{setCountry("CN")}} style={{cursor:"pointer"}}>China</div></li>
+  <ul className="dropdown-menu">
+      <li><div className="dropdown-item" onClick={()=>{setCountry("IN")}} style={{cursor:"pointer"}}>India</div></li>
+      <li><div className="dropdown-item" onClick={()=>{setCountry("US")}} style={{cursor:"pointer"}}>United States</div></li>
+      <li><div className="dropdown-item" onClick={()=>{setCountry("GB")}} style={{cursor:"pointer"}}>United Kingdom</div></li>
+      <li><div className="dropdown-item" onClick={()=>{setCountry("CA")}} style={{cursor:"pointer"}}>Canada</div></li>
+      <li><div className="dropdown-item" onClick={()=>{setCountry("DE")}} style={{cursor:"pointer"}}>Germany</div></li>
+      <li><div className="dropdown-item" onClick={()=>{setCountry("FR")}} style={{cursor:"pointer"}}>France</div></li>
+      <li><div className="dropdown-item" onClick={()=>{setCountry("IT")}} style={{cursor:"pointer"}}>Italy</div></li>
+      <li><div className="dropdown-item" onClick={()=>{setCountry("AU")}} style={{cursor:"pointer"}}>Australia</div></li>
+      <li><div className="dropdown-item" onClick={()=>{setCountry("JP")}} style={{cursor:"pointer"}}>Japan</div></li>
+      <li><div className="dropdown-item" onClick={()=>{setCountry("CN")}} style={{cursor:"pointer"}}>China</div></li>
   </ul>
 </div>
 
